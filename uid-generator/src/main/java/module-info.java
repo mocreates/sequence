8b114efc068f54d285f8f9a6bd14c47e11674431
Qimiao Chen <chenqimiao1994@126.com>
2025-05-08 12:35:23 +0800

    upgrade jdk version

---
+++ uid-generator/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author Qimiao Chen
+ * @since 2025/5/8
+ **/
+module com.github.mocreates {
+    requires java.management;
+    requires java.sql;
+
+    exports io.github.mocreates.config;
+    exports io.github.mocreates.exception;
+}
